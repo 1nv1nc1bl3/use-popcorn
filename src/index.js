b480cc9746bb68c2b3d779d7f5043bb6e09150ce
Alkis <alkis.k1972@gmail.com>
2025-10-07 21:09:19 +0300

    Finished reusable stars component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 import StarRating from './StarRating';
 // import App from './App';
+import { useState } from 'react';
+
+function Test() {
+    const [movieRating, setMovieRating] = useState(0);
+    return (
+        <div>
+            <StarRating
+                color='blue'
+                maxRating={10}
+                onSetRating={setMovieRating}
+            />
+            <p>This movie was rated {movieRating} stars</p>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <StarRating maxRating={5} />
+        <StarRating
+            maxRating={'sss'}
+            messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+        />
+        <StarRating size={24} color='red' className='test' defaultRating={3} />
+        <Test />
 
         {/* <App /> */}
     </React.StrictMode>
